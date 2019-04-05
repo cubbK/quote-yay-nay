ef0827e39bc9d7d8aadb58fad5a33dfb10e5422d
cubbk <danpopa1998@gmail.com>
2019-04-05 23:21:42 +0300

    add dynamodb local

--- src/helpers/useDynamoDb.js
+++ src/helpers/useDynamoDb.js
@@ -1,3 +1,4 @@
-const AWS = require('aws-sdk');
+var dynamodb = require("serverless-dynamodb-client");
+var docClient = dynamodb.doc; // return an instance of new AWS.DynamoDB.DocumentClient()
 
-module.exports = () => new AWS.DynamoDB.DocumentClient()
+module.exports = () => docClient;
--- src/quote.js
+++ src/quote.js
@@ -1,3 +1,38 @@
-module.exports.add = (event, context, callback) => {
+const useDynamoDb = require("./helpers/useDynamoDb");
+const uuid = require("uuid")
+const db = useDynamoDb();
+
+module.exports.create = async (event) => {
+  const params = {
+    TableName: 'quotes',
+    Item: {
+      id: uuid.v1(),
+      quote: "quote 1"
+    }
+  }
+  
+
+  try {
+    const result = await db.put(params).promise()
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'added',
+        input: event,
+        result: result
+      }),
+    };
+  } catch ( err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'Error',
+        input: event,
+        error: err
+      }),
+    };
+  }
+
+  
   
 }
