47495a3352d4c9378aa124461b55acb395361759
cubbk <danpopa1998@gmail.com>
2019-04-06 00:48:36 +0300

    add quote from the body

--- src/quote.js
+++ src/quote.js
@@ -1,38 +1,40 @@
 const useDynamoDb = require("./helpers/useDynamoDb");
-const uuid = require("uuid")
+const uuid = require("uuid");
 const db = useDynamoDb();
 
-module.exports.create = async (event) => {
-  const params = {
-    TableName: 'quotes',
-    Item: {
-      id: uuid.v1(),
-      quote: "quote 1"
-    }
+module.exports.create = async event => {
+  const body = JSON.parse(event.body)
+  const item = {
+    id: uuid.v1(),
+    quote: body.quote,
+    upvotes: 0,
+    downvotes: 0
   }
-  
+
+  const params = {
+    TableName: "quotes",
+    Item: item
+  };
 
   try {
-    const result = await db.put(params).promise()
+
+    await db.put(params).promise();
+
     return {
       statusCode: 200,
       body: JSON.stringify({
-        message: 'added',
-        input: event,
-        result: result
-      }),
+        message: "added",
+        input: event
+      })
     };
-  } catch ( err) {
+  } catch (err) {
     return {
       statusCode: 500,
       body: JSON.stringify({
-        message: 'Error',
+        message: "Error",
         input: event,
         error: err
-      }),
+      })
     };
   }
-
-  
-  
-}
+};
