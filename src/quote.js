676898814e31c526673eeee7c295a935efa069bb
cubbk <danpopa1998@gmail.com>
2019-04-14 23:01:31 +0300

    add setQuoteOfTheDay

--- src/quote.js
+++ src/quote.js
@@ -1,41 +1,65 @@
 const useDynamoDb = require("./helpers/useDynamoDb");
 const uuid = require("uuid");
-const enhanceWithMiddy = require("./helpers/enhanceWithMiddy")
-const createError = require('http-errors')
+const enhanceWithMiddy = require("./helpers/enhanceWithMiddy");
+const createError = require("http-errors");
+const axios = require("axios");
 
 const db = useDynamoDb();
 
-/*
- * shas
- *
- * @param {string} text
- */
-const createQuote = async event => {  
-  console.log(typeof event.body);
-  return createError(402, 'Please login to view this page.')
-  const item = {
-    id: uuid.v1(),
-    text: event.body.text,
-    upvotes: 0,
-    downvotes: 0
-  };
+// Quote example
+// "quote": {
+//   "id": 8889,
+//   "dialogue": false,
+//   "private": false,
+//   "tags": [
+//       "car"
+//   ],
+//   "url": "https://favqs.com/quotes/bill-watterson/8889-shutting-off-t-",
+//   "favorites_count": 0,
+//   "upvotes_count": 1,
+//   "downvotes_count": 0,
+//   "author": "Bill Watterson",
+//   "author_permalink": "bill-watterson",
+//   "body": "Shutting off the thought process is not rejuvenating the mind; it's like a car battery - it recharges by running."
+// }
 
-  const params = {
-    TableName: "quotes",
-    Item: item
-  };
+async function createQuote(quote) {
+  // return createError(402, 'Please login to view this page.')
+  try {
+    const item = {
+      id: quote.id.toString(),
+      data: quote,
+      upvotes: 0,
+      downvotes: 0
+    };
 
-  // await db.put(params).promise();
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "added",
-      quote: "123"
-    })
-  };
-};
+    const params = {
+      TableName: "quotes",
+      Item: item
+    };
+
+    await db.put(params).promise();
+  } catch (err) {
+    throw new Error(err.message);
+  }
+}
+
+async function setQuoteOfTheDay(event) {
+  try {
+    const request = await axios.get("https://favqs.com/api/qotd");
+    const quote = request.data.quote;
+    console.log(quote)
+    await createQuote(quote);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Successfully added the quote.", quote })
+    };
+  } catch (err) {
+    console.log(err)
+    return createError(500, 'Error');
+  }
+}
 
 // Let's "middyfy" our handler, then we will be able to attach middlewares to it
 
-
-module.exports.create = enhanceWithMiddy(createQuote)
+module.exports.setQuoteOfTheDay = enhanceWithMiddy(setQuoteOfTheDay);
