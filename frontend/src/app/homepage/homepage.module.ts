42b24c5331d16bfeb751f5d1614c64a89cbae5db
cubbk <danpopa1998@gmail.com>
2019-04-22 18:21:31 +0300

    style date on quote-component

--- frontend/src/app/homepage/homepage.module.ts
+++ frontend/src/app/homepage/homepage.module.ts
@@ -4,9 +4,10 @@ import { HomepageComponent } from './homepage.component';
 import { QuoteCardComponent } from './quote-card/quote-card.component';
 import { MaterialModule } from '../material.module';
 import { MatCardModule } from '@angular/material';
+import { LoginCardComponent } from './login-card/login-card.component';
 
 @NgModule({
-  declarations: [HomepageComponent, QuoteCardComponent],
+  declarations: [HomepageComponent, QuoteCardComponent, LoginCardComponent],
   imports: [
     CommonModule,
     MaterialModule,
